8b865b3cd32599af21870fe7f4c1b2105475e411
MAC <e.oyagbarha@outlook.com>
2023-11-08 13:25:19 +0100

    New coach

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Man man = new Man();
+//        newClass.name = "Big man";
+        man.getName();
+        man.setName("Ernest");
+
+        System.out.println(man);
+
+        man.setName("Shaka");
+
+        System.out.println(man);
+
+    }
+
+}
---
+++ src/Man.java
@@ -0,0 +1,49 @@
+public class Man {
+    private String name;
+    private String color;
+    private int age;
+    private int height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Man{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
+
